95f9dc9084a9d115981dff28a997aa84614354fe
Dhiaulhaq08 <dhiaulhaq11@gmail.com>
2024-10-15 08:37:26 +0700

    menambah url baru

--- percobaan.js
+++ percobaan.js
@@ -4,9 +4,25 @@ const http = require ('http')
 
 http.createServer ( function(request,response){ 
     response.writeHead(200, {'Content-type' : 'text/html'})
-    response.end(
-        `<h1>Selamat datang `
-    )
+    //halaman utama
+    if ( request.url == '/'){ 
+        response.end (`<h1>Selamat datang ngab`)
+    }
+    else if ( request.url == '/profil'){ 
+        response.end(
+        `<ul>
+            <li>Nama : lengkap </li> 
+            <li>Tanggal : Lahir </li>
+        </ul>`
+        )
+    }
+    // menangani halaman yang tidak ada
+    else { 
+        response.end ( 
+            `<h1>404 : Halaman Tidak ditemukan</h1>`
+        )
+    }
+   
 }).listen (3000, function() { 
     console.log(`server sudah nyala bung, buka http://.localhost:3000`)
 })
